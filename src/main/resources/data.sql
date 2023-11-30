2e6e9a0f23b79fcd878a76692a8838a74d16e6bb
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-11-30 15:08:02 +0100

    test data.sql script executed by spring

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,4 +12,4 @@ INSERT INTO employees (first_name, last_name, mail, password) VALUES
   ('Laurent', 'GINA', 'laurentgina@mail.com', 'laurent'),
   ('Sophie', 'FONCEK', 'sophiefoncek@mail.com', 'sophie'),
   ('Agathe', 'FEELING', 'agathefeeling@mail.com', 'agathe');
-  /*commit;*/
+ /* commit;*/
